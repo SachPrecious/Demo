7246997f0b9440f2e4a2ca1bb10214e42f5aa785
sachithra-manamperi <manamper-se18028@stu.kln.ac.lk>
2021-09-20 21:11:04 +0530

    Commit to the main

--- ElekStore/admin_area/index.php
+++ ElekStore/admin_area/index.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>insert_product</title>
     <link rel="stylesheet" href="Style/style.css">
 </head>
 <body>
     
-
-
    <div class="main_wrapper">
 
    <div id="header"></div>
-
-
 
    <div id="right">
        <h2 style="text-align: center; color:aliceblue;">Manage content</h2>
@@ -29,14 +25,10 @@
        <a href="index.php?view_customers" >View Customers</a>
        <a href="index.php?view_orders" >View Orders</a>
        <a href="index.php?view_payments" >View Payments</a>
-       <a href="index.php?insert_product" >Admin Logout</a>
-
-
-   
+       <a href="index.php?insert_product" >Admin Logout</a>   
 
    </div>
-   
-   
+     
    
    <div id="left">
    <?php 
@@ -46,15 +38,11 @@
    }
    if(isset($_GET['view_products'])){
     include("view_products.php");
-
+   }
     if(isset($_GET['edit_pro'])){
         include("edit_pro.php");
     
-}
-
-
-
-
+    }
 
    ?>
 
--- ElekStore/insert_product.php
+++ ElekStore/insert_product.php
@@ -1,10 +1,21 @@
+
+
+
 <!DOCTYPE html>
+<?php
+
+include("DatabaseConnection.php");
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inserting Products</title>
+    
+  <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>tinymce.init({ selector:'textarea' });</script>
+
 </head>
 <body bgcolor="skyblue">
 
@@ -17,37 +28,88 @@
     
     <tr>
         <td align="right"><b>Product Title</b></td>
-        <td><input type="text" name="product_title" id=""></td>
+        <td><input type="text" name="product_title" id="" size="50"></td>
     </tr>
 
     <tr>
         <td align="right"><b>Product Category</b> </td>
-        <td><input type="text" name="product_title" id=""></td>
+     
+        <td>
+             <select name="product_cat" id="">
+                <option value="">Select Category</option>
+                 <?php
+                 
+                $get_cats="select * from categories";
+                $run_categories=mysqli_query($con,$get_cats);
+
+                 while($row_categories=mysqli_fetch_array($run_categories)){
+
+                    $cat_id=$row_categories['cat_id'];
+                    $cat_title=$row_categories['cat_title'];
+                    
+                   echo "<option value='$cat_id'>$cat_title</option>";
+
+                 }
+                ?>
+             </select>
+
+        </td>
+
     </tr>
     <tr>
         <td align="right"><b>Product Brand</b> </td>
-        <td><input type="text" name="product_title" id=""></td>
+        <td>
+        <select name="product_brand" id="">
+                <option value="">Select Brand</option>
+                 <?php
+                 
+                $get_brands="select * from brands";
+                $run_brands=mysqli_query($con,$get_brands);
+
+                 while($row_brands=mysqli_fetch_array($run_brands)){
+
+                    $brand_id=$row_brands['brand_id'];
+                    $brand_title=$row_brands['brand_title'];
+                    
+                   echo "<option value='$brand_id'>$brand_title</option>";
+
+                 }
+                ?>
+             </select>
+        
+        
+        
+        
+        
+        
+        </td>
     </tr>
     
     
     <tr>
         <td align="right"><b>Product Image</b> </td>
-        <td><input type="text" name="product_title" id=""></td>
+        <td><input type="file" name="product_image" id=""></td>
     </tr>
     <tr>
         <td align="right"><b>Product Price</b> </td>
-        <td><input type="text" name="product_title" id=""></td>
+        <td><input type="text" name="product_price" id=""></td>
     </tr>
     <tr>
         <td align="right"><b>Product Description</b> </td>
-        <td><input type="text" name="product_title" id=""></td>
+        <td>
+            <textarea name="product_desc" id="" cols="35" rows="8"></textarea>
+        </td>
     </tr>
     <tr>
         <td align="right"><b>Product Keywords</b> </td>
-        <td><input type="text" name="product_title" id=""></td>
+        <td><input type="text" name="product_keywords" id=""></td>
     </tr>
     <tr>
-        <td><input type="button" value="Insert Now"></td>
+        <td align="right"><b>Quantity</b> </td>
+        <td><input type="text" name="product_quantity" id=""></td>
+    </tr>
+    <tr>
+        <td colspan="7" align="center"><input type="submit" name="insert_post" value="Insert Now"></td>
     </tr>   
       
       
@@ -69,4 +131,7 @@
 
 
 </body>
+
+
+
 </html>
