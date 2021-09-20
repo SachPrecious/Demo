bd809bb8fd8151f33273f45552547b8c26241cce
sachithra-manamperi <manamper-se18028@stu.kln.ac.lk>
2021-09-20 17:10:25 +0530

    With The Insert Products to main part2

--- ElekStore/insert_product.php
+++ ElekStore/insert_product.php
@@ -8,13 +8,48 @@
 </head>
 <body bgcolor="skyblue">
 
-<form action="insert_product.php" method="post">
+<form action="insert_product.php" method="post" enctype="multipart/form-data">
 
 <table align="center" width="750" border="2" bgcolor="orange">
     <tr align="center">
-
+       <td colspan="7"><h2>Insert New Products Here</h2></td>
+    </tr>
+    
+    <tr>
+        <td align="right"><b>Product Title</b></td>
+        <td><input type="text" name="product_title" id=""></td>
     </tr>
 
+    <tr>
+        <td align="right"><b>Product Category</b> </td>
+        <td><input type="text" name="product_title" id=""></td>
+    </tr>
+    <tr>
+        <td align="right"><b>Product Brand</b> </td>
+        <td><input type="text" name="product_title" id=""></td>
+    </tr>
+    
+    
+    <tr>
+        <td align="right"><b>Product Image</b> </td>
+        <td><input type="text" name="product_title" id=""></td>
+    </tr>
+    <tr>
+        <td align="right"><b>Product Price</b> </td>
+        <td><input type="text" name="product_title" id=""></td>
+    </tr>
+    <tr>
+        <td align="right"><b>Product Description</b> </td>
+        <td><input type="text" name="product_title" id=""></td>
+    </tr>
+    <tr>
+        <td align="right"><b>Product Keywords</b> </td>
+        <td><input type="text" name="product_title" id=""></td>
+    </tr>
+    <tr>
+        <td><input type="button" value="Insert Now"></td>
+    </tr>   
+      
       
 
 
